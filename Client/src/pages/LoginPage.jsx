720d1fcc3ce2fdeeef0df3f1129c35bcbb68098f
Nomad-soul00 <sahilahamed813@gmail.com>
2025-10-07 01:09:57 +0530

    login states

--- Client/src/pages/LoginPage.jsx
+++ Client/src/pages/LoginPage.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import assets from '../assets/assets'
 
 const LoginPage = () => {
+  const [currentState, setcurrentState] = useState('Sign up');
+  const [fullName, setFullName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [bio, setBio] = useState('');
+  const [isDataSubmitted, setIsDataSubmitted] = useState(false);
+  
+
   return (
     <div className='min-h-screen bg-cover bg-center flex items-center justify-center gap-8 sm:justify-evenly max-sm:flex-col backdrop-blur-2xl'>
+
+      {/* left */}
+      <img src={assets.logo_big} className='w-[min(30vw,250px)]' alt="" />
+      {/* right */}
+      <from className="border-2 bg-white/8 text-white border-gray-500 p-6 flex flex-col gap-6 rounded-lg shadow-lg">
+        <h2 className='flex font-medium text-2xl justify-between items-center'>
+          {currentState}
+          <img src={assets.arrow_icon} alt="" className='cursor-pointer w-5' />
+        </h2>
+
+        {currentState === 'Sign up' && (
+          <input type="text" className='p-2 border border-gray-500 rounded-md focus:outline-none' placeholder='Full Name' required />
+        )}
+      </from>
 
     </div>
   )
