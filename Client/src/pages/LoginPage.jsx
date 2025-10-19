116fd6f539daa6ea134d71f47357c521d3213d08
Nomad-soul00 <sahilahamed813@gmail.com>
2025-10-19 18:15:04 +0530

    Login page

--- Client/src/pages/LoginPage.jsx
+++ Client/src/pages/LoginPage.jsx
@@ -9,6 +9,14 @@ const LoginPage = () => {
   const [bio, setBio] = useState('');
   const [isDataSubmitted, setIsDataSubmitted] = useState(false);
   
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+
+    if(currentState === 'Sign up' && !isDataSubmitted){
+      setIsDataSubmitted(true);
+      return;
+    }
+  }
 
   return (
     <div className='min-h-screen bg-cover bg-center flex items-center justify-center gap-8 sm:justify-evenly max-sm:flex-col backdrop-blur-2xl'>
@@ -16,10 +24,11 @@ const LoginPage = () => {
       {/* left */}
       <img src={assets.logo_big} className='w-[min(30vw,250px)]' alt="" />
       {/* right */}
-      <from className="border-2 bg-white/8 text-white border-gray-500 p-6 flex flex-col gap-6 rounded-lg shadow-lg">
+      <form onSubmit={onSubmitHandler} className="border-2 bg-white/8 text-white border-gray-500 p-6 flex flex-col gap-6 rounded-lg shadow-lg">
         <h2 className='flex font-medium text-2xl justify-between items-center'>
           {currentState}
-          <img src={assets.arrow_icon} alt="" className='cursor-pointer w-5' />
+          {isDataSubmitted && <img src={assets.arrow_icon} onClick={()=>{setIsDataSubmitted(false)}} alt="" className='cursor-pointer w-5'  />
+}
         </h2>
 
         {currentState === 'Sign up' && !isDataSubmitted && (
@@ -35,24 +44,30 @@ const LoginPage = () => {
         )} 
 
         {currentState === 'Sign up' && isDataSubmitted && (
-          <textarea onChange={e=>{setBio(e.target.value)}} value={bio} rows={4} className='p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2  focus:ring-indigo-500' placeholder='Provide a short bio...'></textarea>
+          <textarea onChange={e=>{setBio(e.target.value)}} value={bio} rows={3} className='p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2  focus:ring-indigo-500' placeholder='Provide a short bio...'></textarea>
         )}
 
-        <button type='submit' className='py-3 bg-gradient-to-r from-purple-400 to-violet-600 text-white rounded-md cursor-pointer'>
+        <button type='submit' className='py-3 bg-gradient-to-r from-purple-400 to-violet-600 text-white rounded-md cursor-pointer active:from-purple-600 active:to-violet-800'>
           {currentState === 'Sign up'? 'Create Account': 'Login'}
           </button>
 
           <div className='flex items-center gap-2 text-sm text-gray-500'>
-            <input type="checkbox" />
+            <input type="checkbox" className='cursor-pointer' />
             <p>Agree to the terms of use & privacy policy</p>
           </div>
 
           <div className='flex flex-col gap-2'>
-            {/* {currentState === ''} */}
-            {/* 1:46 */}
+            {currentState === 'Sign up' ? (
+              <p className='text-sm text-gray-600'>Already have an account? <span onClick={()=>{setcurrentState('Login'); setIsDataSubmitted(false)}} className='font-medium text-violet-500 cursor-pointer'>Login here</span></p>
+            ) : (
+              <p className='text-sm text-gray-600'>
+                Create an account <span onClick={()=>{setcurrentState('Sign up'); setIsDataSubmitted(false)}} className='font-medium text-violet-500 cursor-pointer'>Click here</span>
+              </p>
+            )}
+           
           </div>
 
-      </from>
+      </form>
 
     </div>
   )
